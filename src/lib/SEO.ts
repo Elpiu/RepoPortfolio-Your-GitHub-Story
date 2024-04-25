e34fef724aa28fbd65a674fdb36a586c09809f61
Elpidio Mazza <49283847+Elpiu@users.noreply.github.com>
2024-04-25 19:12:09 +0200

    keywords SEO
    keywords SEO

--- src/lib/SEO.ts
+++ src/lib/SEO.ts
@@ -3,8 +3,15 @@ import {Metadata} from "next";
 export const getSEOTagsLayout = (): Metadata => {
   return {
     title: "Elpidio | Personal Portfolio",
-    description: "Elpidio is a full-stack developer with 4 year of experience.",
+    description: "I am a full stack developer with 4 year of experience, based in Salerno, Italia, with expertise in Angular, Java, Spring, Typescript. I specialize in building dynamic and responsive web applications. I'm a problem solver, a developer with a strong curriculum, I'm looking for a job or freelancer opportunities. I'm graduated from Università degli Studi di Salerno, Fisciano.",
+    keywords: [
+      "Full stack developer", "web development", "front-end", "back-end",
+      "Angular", "React", "Node.js", "Spring", "Salerno", "Italia",
+      "curriculum", "CV", "lavoro", "freelancer", "problem solver", "developer", "università fisciano",
+      "unisa", "Uni", "IT", "information computer technology", "GitHub"
+    ],
     creator: "Elpidio Mazza",
+    authors: [{name: "Elpidio Mazza"}],
     verification: {
       google: "lDBVEl4aNsiOn2GFnlTBVO8G_skMhZE8xPEioEFmEeE" // Change with yours
     },
@@ -12,3 +19,4 @@ export const getSEOTagsLayout = (): Metadata => {
     publisher: "Elpidio Mazza",
   }
 }
+
