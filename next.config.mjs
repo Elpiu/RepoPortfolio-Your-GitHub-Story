9d71a720014fc1b4277f3b1b37e6acb88131f5c1
Elpidio Mazza <49283847+Elpiu@users.noreply.github.com>
2024-04-27 11:37:39 +0200

    BuyMeCoffeeWidget
    BuyMeCoffeeWidget

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@
 //const isProd = process.env.NODE_ENV = 'production'
 
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   output: "export",
   distDir: 'docs',
   //basePath: isProd ? '/RepoPortfolio-Your-GitHub-Story' : '',
--- src/lib/BuyMeCoffeeWidget.tsx
+++ src/lib/BuyMeCoffeeWidget.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 
-//Change here the script
+//1) Change here the script
+//2) add defer at the end | defer ></script>
 export const BuyMeCoffeeWidget = () => {
 
   return <>
     <div className="bmc-widget-container">
       <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
               data-id="elpiuudeveloper" data-description="Support me on Buy me a coffee!" data-message=""
-              data-color="#5F7FFF" data-position="left" data-x_margin="18" data-y_margin="18"></script>
+              data-color="#5F7FFF" data-position="left" data-x_margin="18" data-y_margin="18" defer ></script>
     </div>
-
-
   </>
 }
